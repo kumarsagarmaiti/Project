d465b0203e9b4851e2f426ac0280e3ac57c0a391
Divyanshu kumar <divyanshupatle123@gmail.com>
2022-09-22 20:24:51 +0530

    created authorization and deletebook api

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -15,29 +15,16 @@ const createBook = async function (req, res) {
 				.status(400)
 				.send({ status: false, message: "Please Provide Data" });
 
-		const requiredFields = [
-			"title",
-			"excerpt",
-			"userId",
-			"ISBN",
-			"category",
-			"subcategory",
-			"releasedAt",
-		];
+		const requiredFields = ["title", "excerpt", "userId", "ISBN", "category", "subcategory", "releasedAt",];
 
 		for (field of requiredFields) {
 			if (!data[field])
-				return res
-					.status(400)
-					.send({ status: false, message: `${field} is missing.` });
+				return res.status(400).send({ status: false, message: `${field} is missing.` });
 		}
 
 		for (field of requiredFields) {
 			if (!isValidString(data[field]))
-				return res.status(400).send({
-					status: false,
-					message: `${field} must contain characters.`,
-				});
+				return res.status(400).send({ status: false, message: `${field} must contain characters.`, });
 		}
 
 		const ISBNregex =
@@ -47,22 +34,15 @@ const createBook = async function (req, res) {
 
 		if (data.reviews) {
 			if (typeof data.reviews !== "number")
-				return res.status(400).send({
-					status: false,
-					message: "Reviews type invalid, should be in number.",
-				});
+				return res.status(400).send({ status: false, message: "Reviews type invalid, should be in number.", });
 		}
 
 		const releasedAtRegex = /([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/;
 		if (!releasedAtRegex.test(data.releasedAt))
-			return res
-				.status(400)
-				.send({ status: false, message: "Invalid Release Date" });
+			return res.status(400).send({ status: false, message: "Invalid Release Date" });
 
 		if (!ObjectId.isValid(data.userId))
-			return res
-				.status(400)
-				.send({ status: false, message: "Invalid ObjectId:UserId" });
+			return res.status(400).send({ status: false, message: "Invalid ObjectId:UserId" });
 
 		const uniqueFields = ["title", "ISBN"];
 		for (field of uniqueFields) {
@@ -87,9 +67,11 @@ const getbooks = async function (req, res) {
 	try {
 		let obj = { isDeleted: false };
 
-		let userId = req.query.userId;
-		let category = req.query.category;
-		let subcategory = req.query.subcategory;
+		// let userId = req.query.userId;
+		// let category = req.query.category;
+		// let subcategory = req.query.subcategory;
+
+		let { userId, category, subcategory } = req.query
 
 		if (userId) {
 			obj.userId = userId;
@@ -101,21 +83,11 @@ const getbooks = async function (req, res) {
 			obj.subcategory = subcategory;
 		}
 
-		let findbook = await Book.find(obj)
-			.select({
-				_id: 1,
-				title: 1,
-				excerpt: 1,
-				userId: 1,
-				category: 1,
-				reviews: 1,
-				releasedAt: 1,
-			})
+		let findbook = await Book.find(obj).select({_id: 1,title: 1,excerpt: 1,userId: 1,category: 1,reviews: 1,releasedAt: 1,})
 			.sort({ title: 1 });
 		if (findbook.length == 0) {
 			return res.status(404).send({ status: false, message: "book not found" });
 		}
-
 		return res
 			.status(200)
 			.send({ status: true, message: "Success", data: findbook });
@@ -128,27 +100,50 @@ const getbooks = async function (req, res) {
 
 const booksbyparam = async function (req, res) {
 	try {
-		let bookId = req.param.bookId;
+		let bookId = req.params.bookId;
 
 		if (!bookId) {
-			return res
-				.status(400)
-				.send({ status: false, message: "bookId is mendatory" });
+			return res.status(400).send({ status: false, message: "bookId is mendatory" });
 		}
 
-		let findbookId = await Book.findById(bookId);
+		let findbookId = await Book.findOne({ _id: bookId, isDeleted: false });
 		if (!findbookId) {
 			return res.status(404).send({ status: false, message: "book not found" });
 		}
 
-		return res
-			.status(200)
-			.send({ status: true, message: "Success", data: findbookId });
+		return res.status(200).send({ status: true, message: "Success", data: findbookId });
 	} catch (err) {
-		return res
-			.status(500)
-			.send({ status: false, message: "Error", error: err.message });
+		return res.status(500).send({ status: false, message: "Error", error: err.message });
 	}
 };
 
-module.exports = { createBook, getbooks, booksbyparam };
+// ### DELETE /books/:bookId
+// - Check if the bookId exists and is not deleted. If it does, mark it deleted and return an HTTP status 200 with a response body with status and message.
+// - If the book document doesn't exist then return an HTTP status of 404 with a body like [this](#error-response-structure)
+
+const deletebook = async function (req, res) {
+	try {
+		
+		let bookId = req.params.bookId;
+		if (!bookId) {
+			return res.status(400).send({ status: false, message: "bookId is mendatory" });
+		}
+
+		let findbook = await Book.findById(bookId)
+		if (!findbook) {
+			return res.status(404).send("blog document doesn't exist");
+		}
+
+		let data = findbook.isDeleted
+		if (data == true) {
+			return res.status(404).send("blog document already deleted");
+		}
+
+		let markdelete = await Book.updateOne({ _id: bookId }, { isDeleted: true ,deletedAt:new Date}, { new: true })
+		res.status(200).send({ status: true, message: "success" })
+	} catch (err) {
+		res.status(500).send({ status: false, message: "Error", error: err.message });
+	}
+}
+
+module.exports = { createBook, getbooks, booksbyparam, deletebook };
