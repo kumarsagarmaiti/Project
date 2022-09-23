ca6b48445546f4aac906a891caa399700b8ecae1
Divyanshu kumar <divyanshupatle123@gmail.com>
2022-09-23 14:53:47 +0530

    put api

--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -1,1 +1,42 @@
-const Review=require("../models/reviewModel")
+const Review = require("../models/reviewModel")
+const Book = require("../models/bookModel");
+
+const putApi = async function (req, res) {
+    try {
+        let { bookId, reviewId } = req.params
+        let findbookid = await Book.findById({ _id: bookId, isDeleted: false })
+        if (!findbookid) {
+            return res.status(404).send({ status: false, message: "book not found" })
+        }
+        let findreviewId = await Review.findById(reviewId)
+        if (!findreviewId) {
+            return res.status(404).send({ status: false, message: "review not found" })
+        }
+
+        let { reviewedBy, rating, review } = req.body
+        if (!req.body) {
+            return res.status(404).send({ status: false, message: "request body can't be empty" })
+        }
+
+
+        let obj = {}
+        if (userId) {
+            obj.reviewedBy = reviewedBy;
+        }
+        if (rating) {
+            obj.rating = rating;
+        }
+        if (review) {
+            obj.review = review;
+        }
+        obj = { reviewedAt: new Date() }
+
+        let markdelete = await Review.updateOne({ _id: reviewId }, obj, { new: true })
+        return res.status(200).send({ status: true, message: "Success", data: markdelete });
+
+    } catch (err) {
+        res.status(500).send({ status: false, message: err.message });
+    }
+}
+
+module.exports = { putApi }
--- src/routes/route.js
+++ src/routes/route.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const userController = require("../controllers/userController");
 const bookController = require("../controllers/bookController");
+const revieeController = require("../controllers/reviewController");
 const userAuth=require("../middleware/auth")
 
 router.use("/books",userAuth.authenticate)
@@ -17,5 +18,6 @@ router.post("/books",userAuth.authorization, bookController.createBook);
 router.get("/books", bookController.getbooks);
 router.get("/books/:bookId", bookController.booksbyparam);
 router.delete("/books/:bookId",userAuth.authorization1, bookController.deletebook);
+router.put("/books/:bookId/review/:reviewId",revieeController.putApi)
 
 module.exports = router;
