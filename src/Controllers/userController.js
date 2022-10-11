e9821fd908402aa5ccb2b763a4d47fa9f3333915
KingK9t8 <ksagarm789@gmail.com>
2022-10-11 11:00:54 +0530

    two apis

--- src/Controllers/userController.js
+++ src/Controllers/userController.js
@@ -1,7 +1,7 @@
 const User = require("../Models/userModel");
 const validate = require("../Utility/validator");
 const bcrypt = require("bcrypt");
-const {uploadFiles}=require("../MIddleware/aws")
+const { uploadFiles } = require("../MIddleware/aws");
 
 const createUser = async function (req, res) {
 	try {
@@ -132,40 +132,72 @@ const createUser = async function (req, res) {
 };
 
 const userLogin = async function (req, res) {
-    try{
-    let body = req.body
-    const { email, password } = body
-   
-    if (validate.isEmptyObject(body)) {
-        return res.status(400).send({ status: false, message: "Data is required to login" }) }
-    
-        if (!validate.isEmptyVar(email)) return res.status(400).send({ status: false, message: "EmailId is mandatory" })
+	try {
+		let body = req.body;
+		const { email, password } = body;
 
-      if (!validate.isValidEmail(email)) {
-            return res.status(400).send({ status: false, message: "Email must be valid" })}; 
-    
-            if (validate.isEmptyVar(password)) return res.status(400).send({ status: false, message: "Password is mandatory" })
+		if (validate.isEmptyObject(body)) {
+			return res
+				.status(400)
+				.send({ status: false, message: "Data is required to login" });
+		}
 
- if (!validate.isValidPassword) {
-          return res.status(400).send({ status: false, message: `Password length should be A Valid Password And Length Should Be in between 8 to 15 ` })}
-              
-     let bcryptpassword = await bcrypt.compare(password, user.password);
-        if (!bcryptpassword) return res.status(401).send({ status: false, message: "Incorrect password" })
-      
-        let user = await User.findOne({ email: email })
-        if (!user) {
-            return res.status(400).send({ status: false, msg: "Invalid credentials or user not exist" })
-        }
+		if (!validate.isEmptyVar(email))
+			return res
+				.status(400)
+				.send({ status: false, message: "EmailId is mandatory" });
 
-        const token = jwt.sign({
-            userId: user._id, iat: new Date().getTime(),
-            exp: 10,
-        }, "Group-10 secret key")
-        return res.status(200).send({ status: true, message: "User login successfull", data: { userId: user._id, token: token } })
-    } catch (error) {
-        res.status(500).send({ status: false, message: error.message })
-    }
+		if (!validate.isValidEmail(email)) {
+			return res
+				.status(400)
+				.send({ status: false, message: "Email must be valid" });
+		}
 
-}
+		if (validate.isEmptyVar(password))
+			return res
+				.status(400)
+				.send({ status: false, message: "Password is mandatory" });
 
-module.exports = { createUser ,userLogin};
+		if (!validate.isValidPassword) {
+			return res
+				.status(400)
+				.send({
+					status: false,
+					message: `Password length should be A Valid Password And Length Should Be in between 8 to 15 `,
+				});
+		}
+
+		let bcryptpassword = await bcrypt.compare(password, user.password);
+		if (!bcryptpassword)
+			return res
+				.status(401)
+				.send({ status: false, message: "Incorrect password" });
+
+		let user = await User.findOne({ email: email });
+		if (!user) {
+			return res
+				.status(400)
+				.send({ status: false, msg: "Invalid credentials or user not exist" });
+		}
+
+		const token = jwt.sign(
+			{
+				userId: user._id,
+				iat: new Date().getTime(),
+				exp: 10,
+			},
+			"Group-10 secret key"
+		);
+		return res
+			.status(200)
+			.send({
+				status: true,
+				message: "User login successfull",
+				data: { userId: user._id, token: token },
+			});
+	} catch (error) {
+		res.status(500).send({ status: false, message: error.message });
+	}
+};
+
+module.exports = { createUser, userLogin };
